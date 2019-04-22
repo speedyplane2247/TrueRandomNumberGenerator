1f575a7d5aa58199679cf5573933da1431506137
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-04-22 16:33:09 -0400

    Create trng.js

---
+++ trng.js
@@ -0,0 +1,9 @@
+function prng(seed) {
+console.log("Please wait while a random number is being generated...")
+x = 4 * seed
+return 4;
+}
+function rng() {
+console.log("Please wait while a random number is generated...")
+return 4;
+}

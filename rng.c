0fd203aabf04d50e1e01488e15c3b1235014ce6d
speedyplane2247 <36555661+speedyplane2247@users.noreply.github.com>
2019-04-22 16:21:36 -0400

    Create rng.c

---
+++ rng.c
@@ -0,0 +1,6 @@
+/*
+Hey Guys, My Names Keith, Yes I Am A Furry, Stop Asking Mid Comp, feel free to ask about that as long as im not in comp! I Have Moved Away From Steam Trading Because Of Losing Around $5000 To Scammers
+*/
+int randomNum () {
+return 4;
+}
